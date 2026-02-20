e388b2f6739bf16b901c0a288ef83b67b6b909fc
himudit <gargmudit662@gmail.com>
2026-02-20 23:46:31 +0530

    updated changes

--- Frontend/src/utils/lenis.ts
+++ Frontend/src/utils/lenis.ts
@@ -12,7 +12,7 @@ export const initLenis = () => {
         gestureOrientation: 'vertical',
         smoothWheel: true,
         wheelMultiplier: 1,
-        smoothTouch: false,
+        syncTouch: false,
         touchMultiplier: 2,
         infinite: false,
     });
